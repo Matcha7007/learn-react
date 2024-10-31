46cc2a06a185d1ed9a8325e6f3fa2ac5c06684b7
Matcha7007 <matchaaddict007@gmail.com>
2024-10-31 22:55:47 +0700

    Create React From Scratch

---
+++ 03-pizza-menu/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+	return <h1>Hello React</h1>;
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
