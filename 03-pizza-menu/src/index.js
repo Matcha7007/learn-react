a54a18b85385a39c0ebcc77179cc7550f5ab863a
Matcha7007 <matchaaddict007@gmail.com>
2024-11-01 15:02:45 +0700

    create pizza menu

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,8 +1,154 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const pizzaData = [
+	{
+		name: "Focaccia",
+		ingredients: "Bread with italian olive oil and rosemary",
+		price: 6,
+		photoName: "pizzas/focaccia.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Margherita",
+		ingredients: "Tomato and mozarella",
+		price: 10,
+		photoName: "pizzas/margherita.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Spinaci",
+		ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+		price: 12,
+		photoName: "pizzas/spinaci.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Funghi",
+		ingredients: "Tomato, mozarella, mushrooms, and onion",
+		price: 12,
+		photoName: "pizzas/funghi.jpg",
+		soldOut: false,
+	},
+	{
+		name: "Pizza Salamino",
+		ingredients: "Tomato, mozarella, and pepperoni",
+		price: 15,
+		photoName: "pizzas/salamino.jpg",
+		soldOut: true,
+	},
+	{
+		name: "Pizza Prosciutto",
+		ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+		price: 18,
+		photoName: "pizzas/prosciutto.jpg",
+		soldOut: false,
+	},
+];
+
+// component react ini terdiri dari data - logic - ui/html
+// contohnya ini ->
+// dalam penulisan nama function jsx ini mesti diawali dengan huruf kapital
+// semua yang ada di dalam function ini data + logic + ui akan dirender dari jsx -> html
+// function App() {
+// di dalam function ini kita bisa menambahkan syntax javascript
+// 	return (
+// nah di return ini adalah html
+// kita juga bisa menggunakan js mode dengan memanggil kurawal {}
+// jadi di dalam {} kita bisa menggunakan syntax js
+// seperti assign variable, logic, function array dll
+// 		<div className="container">
+// 			<Header />
+// 			<Menu />
+// 			<Footer />
+// 		</div>
+// 	);
+// }
 
 function App() {
-	return <h1>Hello React</h1>;
+	return (
+		<div className="container">
+			<Header />
+			<Menu />
+			<Footer />
+		</div>
+	);
+}
+
+function Header() {
+	// const style = { color: "red", fontSize: "48px", textTransform: "uppercase" };
+	const style = {};
+	return (
+		<header className="header">
+			<h1 style={style}>Fast React Piza Co.</h1>
+		</header>
+	);
+}
+
+function Menu() {
+	const pizzas = pizzaData;
+	const numPizzas = pizzas.length;
+	return (
+		<main className="menu">
+			<h2>Our Menu</h2>
+			{/* render elemen dengan kondisi sort circuit dengan operator logical && */}
+			{/* ini akan merender elemen di sebelah kanan && jika di kiri bernilai true */}
+			{/* {numPizzas > 0 && (
+				<ul className="pizzas">
+					{pizzas.map((pizza) => (
+						<Pizza pizzaObj={pizza} key={pizza.name} />
+					))}
+				</ul>
+			)} */}
+
+			{/* render elemen dengan kondisi ternary operator */}
+			{/* ini render seperti if else jadi kalo true akan render elemen A dan jika false akan render elemen B */}
+			{numPizzas > 0 ? (
+				<ul className="pizzas">
+					{pizzas.map((pizza) => (
+						<Pizza pizzaObj={pizza} key={pizza.name} />
+					))}
+				</ul>
+			) : (
+				<p>We're still working on our menu. Please come back later :)</p>
+			)}
+		</main>
+	);
+}
+
+function Pizza(props) {
+	return (
+		<li className="pizza">
+			<img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
+			<div>
+				<h3>{props.pizzaObj.name}</h3>
+				<p>{props.pizzaObj.ingredients}</p>
+				<span>{props.pizzaObj.price}</span>
+			</div>
+		</li>
+	);
+}
+
+function Footer() {
+	const hour = new Date().getHours();
+	const openHour = 10;
+	const closeHour = 22;
+	const isOpen = hour >= openHour && hour <= closeHour;
+	return (
+		<footer className="footer">
+			{isOpen ? (
+				<div className="order">
+					<p>We're open until {closeHour}:00. Come visit us or order online.</p>
+					<button className="btn">Order</button>
+				</div>
+			) : (
+				<p>
+					We're happy to welcome you between {openHour}:00 to {closeHour}:00
+				</p>
+			)}
+		</footer>
+	);
 }
 
 const container = document.getElementById("root");
