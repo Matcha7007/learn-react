a5af511426c8a0f901d7bb061e8903b0cc8e22aa
Matcha7007 <matchaaddict007@gmail.com>
2024-11-04 21:37:30 +0700

    Lifting State Up

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -1,14 +1,28 @@
+import { useState } from "react";
+
 const initialItems = [
 	{ id: 1, description: "Passports", quantity: 2, packed: false },
 	{ id: 2, description: "Socks", quantity: 12, packed: true },
 ];
 
 export default function App() {
+	const [items, setItems] = useState([]);
+
+	function handleAddItems(item) {
+		setItems((items) => [...items, item]);
+	}
+
+	// karena component Form dan PackingList ini perlu state yang sama
+	// sedangkan posisinya mereka siblings atau sejajar bukan parent - child
+	// jadi perlu buat state nya di parent component dari Form dan PackingList
+	// lalu untuk mengambil data dari Form dihandle oleh function handleAddItems, yang mana akan dipanggil dari Form saat submit untuk di-setItems
+	// nah sedangkan array dari items ini dikirim ke PackingList untuk dirender
+
 	return (
 		<div className="app">
 			<Logo />
-			<Form />
-			<PackingList />
+			<Form onAddItems={handleAddItems} />
+			<PackingList items={items} />
 			<Stats />
 		</div>
 	);
@@ -18,19 +32,56 @@ function Logo() {
 	return <h1>🥳 Far Away 🙂‍↔️</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
+	const [quantity, setQuantity] = useState(1);
+	const [description, setDescription] = useState("");
+
+	function handleSubmit(e) {
+		// e.preventDefault() untuk mencegah reload page / lainnya
+		e.preventDefault();
+		if (!description) return;
+
+		const newItem = {
+			description: description,
+			quantity: quantity,
+			packed: false,
+			id: Date.now(),
+		};
+
+		onAddItems(newItem);
+
+		setDescription("");
+		setQuantity(1);
+	}
+
 	return (
-		<div className="add-form">
+		<form className="add-form" onSubmit={handleSubmit}>
 			<h3>What do you need for your 🤩 trip?</h3>
-		</div>
+			<select
+				value={quantity}
+				onChange={(e) => setQuantity(Number(e.target.value))}>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option key={num} value={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
+			<button>Add</button>
+		</form>
 	);
 }
 
-function PackingList() {
+function PackingList({ items }) {
 	return (
 		<div className="list">
 			<ul>
-				{initialItems.map((item) => (
+				{items.map((item) => (
 					<Item key={item.id} item={item} />
 				))}
 			</ul>
