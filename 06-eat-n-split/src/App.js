b21fe9b2a63e4f2f988d97df9b88df97e3610dca
Matcha7007 <matchaaddict007@gmail.com>
2024-11-07 17:14:51 +0700

    Add form friends & split bill

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -24,7 +24,10 @@ export default function App() {
 		<div className="app">
 			<div className="sidebar">
 				<FriendsList />
+				<FormAddFriend />
+				<Button>Add Friend</Button>
 			</div>
+			<FormSplitBill />
 		</div>
 	);
 }
@@ -57,6 +60,50 @@ function Friend({ friend }) {
 				</p>
 			)}
 			{friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+			<Button>Select</Button>
 		</li>
+	);
+}
+
+function Button({ children }) {
+	return <button className="button">{children}</button>;
+}
+
+function FormAddFriend() {
+	return (
+		<form className="form-add-friend">
+			<label>G Friend Name:</label>
+			<input type="text" />
+
+			<label>G Image Url</label>
+			<input type="text" />
+
+			<Button>Add</Button>
+		</form>
+	);
+}
+
+function FormSplitBill() {
+	return (
+		<form className="form-split-bill">
+			<h2>Split a bill with X</h2>
+			<label>G Bill value</label>
+			<input type="text" />
+
+			<label>G Your expense</label>
+			<input type="text" />
+
+			<label>G X's expense</label>
+			<input type="text" disabled />
+
+			<label>G Who is the paying?</label>
+			<select>
+				<option value="user">You</option>
+				<option value="friend">X</option>
+			</select>
+
+			<Button>Split bill</Button>
+		</form>
 	);
 }
