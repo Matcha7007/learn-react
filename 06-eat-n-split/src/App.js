3f0f8a940a5ae4fd0e7f0ec339db659361fe9f12
Matcha7007 <matchaaddict007@gmail.com>
2024-11-07 18:44:03 +0700

    Add logic and stat at form add friend

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
 	{
 		id: 118836,
@@ -19,21 +21,42 @@ const initialFriends = [
 	},
 ];
 
+function Button({ children, onClick }) {
+	return (
+		<button className="button" onClick={onClick}>
+			{children}
+		</button>
+	);
+}
+
 export default function App() {
+	const [friends, setFriends] = useState(initialFriends);
+	const [showFormAddFriend, setShowFormAddFriend] = useState(false);
+
+	function handleShow() {
+		setShowFormAddFriend((show) => !show);
+	}
+
+	function handleAddFriend(friend) {
+		setFriends((curFriend) => [...curFriend, friend]);
+		setShowFormAddFriend(false);
+	}
+
 	return (
 		<div className="app">
 			<div className="sidebar">
-				<FriendsList />
-				<FormAddFriend />
-				<Button>Add Friend</Button>
+				<FriendsList friends={friends} />
+				{showFormAddFriend && <FormAddFriend onAddFriend={handleAddFriend} />}
+				<Button onClick={handleShow}>
+					{showFormAddFriend ? "close" : "Add Friend"}
+				</Button>
 			</div>
 			<FormSplitBill />
 		</div>
 	);
 }
 
-function FriendsList() {
-	const friends = initialFriends;
+function FriendsList({ friends }) {
 	return (
 		<ul>
 			{friends.map((friend) => (
@@ -66,18 +89,42 @@ function Friend({ friend }) {
 	);
 }
 
-function Button({ children }) {
-	return <button className="button">{children}</button>;
-}
+function FormAddFriend({ onAddFriend }) {
+	const [name, setName] = useState("");
+	const [image, setImage] = useState("https://i.pravatar.cc/48");
 
-function FormAddFriend() {
+	function handleSubmit(e) {
+		e.preventDefault();
+		if (!name || !image) return;
+
+		const id = crypto.randomUUID();
+
+		const newFriend = {
+			id,
+			name,
+			image: `${image}?=${id}`,
+			balance: 0,
+		};
+		onAddFriend(newFriend);
+		setName("");
+		setImage("https://i.pravatar.cc/48");
+	}
+
 	return (
-		<form className="form-add-friend">
-			<label>G Friend Name:</label>
-			<input type="text" />
+		<form className="form-add-friend" onSubmit={handleSubmit}>
+			<label>G Friend Name</label>
+			<input
+				type="text"
+				value={name}
+				onChange={(e) => setName(e.target.value)}
+			/>
 
 			<label>G Image Url</label>
-			<input type="text" />
+			<input
+				type="text"
+				value={image}
+				onChange={(e) => setImage(e.target.value)}
+			/>
 
 			<Button>Add</Button>
 		</form>
