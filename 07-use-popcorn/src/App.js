dae34307ae935c6a7e7b3435fb225adfa4733fa7
Matcha7007 <matchaaddict007@gmail.com>
2024-11-09 13:10:34 +0700

    Drilling Props

--- 07-use-popcorn/src/App.js
+++ 07-use-popcorn/src/App.js
@@ -51,28 +51,30 @@ const average = (arr) =>
 	arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
 export default function App() {
+	const [movies, setMovies] = useState(tempMovieData);
+
 	return (
 		<>
-			<NavBar />
-			<Main />
+			<NavBar movies={movies} />
+			<Main movies={movies} />
 		</>
 	);
 }
 
-function NavBar() {
+function NavBar({ movies }) {
 	return (
 		<nav className="nav-bar">
 			<Logo />
 			<Search />
-			<NumResult />
+			<NumResult movies={movies} />
 		</nav>
 	);
 }
 
-function NumResult() {
+function NumResult({ movies }) {
 	return (
 		<p className="num-results">
-			Found <strong>X</strong> results
+			Found <strong>{movies.length}</strong> results
 		</p>
 	);
 }
@@ -100,10 +102,10 @@ function Search() {
 	);
 }
 
-function Main() {
+function Main({ movies }) {
 	return (
 		<main className="main">
-			<ListBox />
+			<ListBox movies={movies} />
 			<WatchedBox />
 		</main>
 	);
@@ -193,7 +195,7 @@ function WatchedSummary({ watched }) {
 	);
 }
 
-function ListBox() {
+function ListBox({ movies }) {
 	const [isOpen1, setIsOpen1] = useState(true);
 
 	return (
@@ -203,14 +205,12 @@ function ListBox() {
 				onClick={() => setIsOpen1((open) => !open)}>
 				{isOpen1 ? "–" : "+"}
 			</button>
-			{isOpen1 && <MovieList />}
+			{isOpen1 && <MovieList movies={movies} />}
 		</div>
 	);
 }
 
-function MovieList() {
-	const [movies, setMovies] = useState(tempMovieData);
-
+function MovieList({ movies }) {
 	return (
 		<ul className="list">
 			{movies?.map((movie) => (
