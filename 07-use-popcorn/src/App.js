34ccd15b6861a560ebc165ab95d948f364ee037e
Matcha7007 <matchaaddict007@gmail.com>
2024-11-09 14:18:05 +0700

    Fixing prop drilling

--- 07-use-popcorn/src/App.js
+++ 07-use-popcorn/src/App.js
@@ -55,18 +55,25 @@ export default function App() {
 
 	return (
 		<>
-			<NavBar movies={movies} />
-			<Main movies={movies} />
+			<NavBar>
+				<Search />
+				<NumResult movies={movies} />
+			</NavBar>
+			<Main>
+				<ListBox>
+					<MovieList movies={movies} />
+				</ListBox>
+				<WatchedBox />
+			</Main>
 		</>
 	);
 }
 
-function NavBar({ movies }) {
+function NavBar({ children }) {
 	return (
 		<nav className="nav-bar">
 			<Logo />
-			<Search />
-			<NumResult movies={movies} />
+			{children}
 		</nav>
 	);
 }
@@ -102,13 +109,8 @@ function Search() {
 	);
 }
 
-function Main({ movies }) {
-	return (
-		<main className="main">
-			<ListBox movies={movies} />
-			<WatchedBox />
-		</main>
-	);
+function Main({ children }) {
+	return <main className="main">{children}</main>;
 }
 
 function WatchedBox() {
@@ -195,7 +197,7 @@ function WatchedSummary({ watched }) {
 	);
 }
 
-function ListBox({ movies }) {
+function ListBox({ children }) {
 	const [isOpen1, setIsOpen1] = useState(true);
 
 	return (
@@ -205,7 +207,7 @@ function ListBox({ movies }) {
 				onClick={() => setIsOpen1((open) => !open)}>
 				{isOpen1 ? "–" : "+"}
 			</button>
-			{isOpen1 && <MovieList movies={movies} />}
+			{isOpen1 && children}
 		</div>
 	);
 }
